c50ba5020954f3eb6b64a4c49e7dff4cca7ad306
agusnicflo <agusnicflores@gmail.com>
2024-12-14 17:15:10 -0300

    arregladoreloj

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "../styles/global.css"
 
 const EditPage = () => {
   const [isActive, setIsActive] = useState(false); // Estado para saber si está activo o no
@@ -9,6 +10,10 @@ const EditPage = () => {
   const [profileImage, setProfileImage] = useState(null);
   const [selectedImages, setSelectedImages] = useState([]);
   const [selectedImageForMobile, setSelectedImageForMobile] = useState(null); // Imagen mostrada en el móvil
+  const [horaInicio, setHoraInicio] = useState("6:00 AM");
+  const [horaFin, setHoraFin] = useState("12:00 PM");
+  const [diaDesde, setDiaDesde] = useState("lunes");
+  const [diaHasta, setDiaHasta] = useState("lunes");
 
   const handleImageClick = (image) => {
     setSelectedImageForMobile(image); // Actualizamos la imagen mostrada en el móvil
@@ -98,7 +103,7 @@ const EditPage = () => {
                   borderRadius: "10px",
                   objectFit: "cover",
                   position: "absolute",
-                  top: "60%",
+                  top: "65%",
                 }}
               />
             )}
@@ -113,24 +118,42 @@ const EditPage = () => {
                   borderRadius: "50%", // Hace que la imagen tenga forma circular
                   objectFit: "cover", // Para que la imagen no se distorsione
                   position: "absolute", // Para posicionar la imagen dentro de la silueta
-                  top: "17%", // Ajusta la posición vertical
+                  top: "12%", // Ajusta la posición vertical
                   left: "50%", // Ajusta la posición horizontal
                   transform: "translate(-50%, -50%)", // Centra la imagen
                 }}
               />
             )}
-            <div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div style={{ position: "absolute", top: "22%", textAlign: "center" }}>
+              <div>
+                <div
+                  style={{
+                    alignItems: "center",
+                    fontSize: "11px",
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                >
+                  <p style={{ fontSize: "18px" }}>Peluqueria</p>
+                  <p>
+                    Abierto de {diaDesde} a {diaHasta}
+                  </p>
+                  <p>
+                    {horaInicio} - {horaFin}
+                  </p>
+                </div>
+              </div>
             </div>
             {isActive && (
               <button
                 style={{
                   width: "80%",
                   padding: "10px",
-                  borderRadius: "5%",
+                  borderRadius: "10px",
                   border: "none",
+                  marginTop: "10px", // Esto mueve los botones hacia arriba
+                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)", // Sombra hacia abajo
+                  fontWeight: "bold"
                 }}
               >
                 Servicios
@@ -141,8 +164,11 @@ const EditPage = () => {
                 style={{
                   width: "80%",
                   padding: "10px",
-                  borderRadius: "5%",
+                  borderRadius: "10px",
                   border: "none",
+                  marginTop: "10px", // Esto mueve los botones hacia arriba
+                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)", // Sombra hacia abajo
+                  fontWeight: "bold"
                 }}
               >
                 Agendar Turnos
@@ -296,7 +322,7 @@ const EditPage = () => {
             ))}
           </div>
         </div>
-        {/* Contenedores vacíos */}
+        {/* Agenda */}
         <div
           style={{
             flex: 1,
@@ -368,42 +394,75 @@ const EditPage = () => {
           <div>
             <div
               style={{
+                margin: "5px",
                 display: "flex",
-                border: "1px solid black",
                 flexDirection: "column",
+                backgroundColor: "#dddfdb",
+                borderRadius: "10px",
+                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)", // Sombra hacia abajo
               }}
             >
-              <label htmlFor="horarioInicio" style={{ margin: "0 10px" }}>
+              <label htmlFor="horarioInicio" style={{ margin: "0 10px", color:"gray" }}>
                 Horarios
               </label>
               <div style={{ display: "flex", alignItems: "center" }}>
                 <div style={{ margin: "5px" }}>
-                  <label htmlFor="desde" style={{ margin: "0 5px" }}>
+                  <label htmlFor="desde" style={{ margin: "0 5px", fontSize:"14px" }}>
                     Desde
                   </label>
                   <select
                     id="horarioInicio"
-                    style={{ marginBottom: "10px", marginLeft: "5px" }}
+                    value={horaInicio}
+                    onChange={(e) => setHoraInicio(e.target.value)}
+                    style={{ marginBottom: "10px", marginLeft: "5px", borderRadius:"5px", border:"none" }}
                   >
-                    <option value="08:00">08:00</option>
-                    <option value="09:00">09:00</option>
-                    <option value="10:00">10:00</option>
-                    <option value="11:00">11:00</option>
-                    <option value="12:00">12:00</option>
+                    <option value="06:00">6:00 am</option>
+                    <option value="07:00">7:00 am</option>
+                    <option value="08:00">8:00 am</option>
+                    <option value="09:00">9:00 am</option>
+                    <option value="10:00">10:00 am</option>
+                    <option value="11:00">11:00 am</option>
+                    <option value="12:00">12:00 am</option>
+                    <option value="13:00">13:00 pm</option>
+                    <option value="14:00">14:00 pm</option>
+                    <option value="15:00">15:00 pm</option>
+                    <option value="16:00">16:00 pm</option>
+                    <option value="17:00">17:00 pm</option>
+                    <option value="18:00">18:00 pm</option>
+                    <option value="19:00">19:00 pm</option>
+                    <option value="20:00">20:00 pm</option>
+                    <option value="21:00">21:00 pm</option>
+                    <option value="22:00">22:00 pm</option>
+                    <option value="23:00">23:00 pm</option>
                     {/* Agrega más opciones de horario según lo necesites */}
                   </select>
                 </div>
-                <div style={{ margin: "5px" }}>
-                  <label style={{ margin: "0 5px" }}>Hasta</label>
+                <div style={{ marginLeft: "5px" }}>
+                  <label style={{ margin: "0 5px", fontSize:"14px" }}>Hasta</label>
                   <select
                     id="horarioFin"
-                    style={{ marginBottom: "10px", marginLeft: "5px" }}
+                    value={horaFin}
+                    onChange={(e) => setHoraFin(e.target.value)} // Actualiza el estado cuando cambia el valor
+                    style={{ marginBottom: "10px", marginLeft: "5px", borderRadius:"5px", border:"none" }}
                   >
-                    <option value="08:30">08:30</option>
-                    <option value="09:30">09:30</option>
-                    <option value="10:30">10:30</option>
-                    <option value="11:30">11:30</option>
-                    <option value="12:30">12:30</option>
+                    <option value="06:00">6:00 am</option>
+                    <option value="07:00">7:00 am</option>
+                    <option value="08:00">8:00 am</option>
+                    <option value="09:00">9:00 am</option>
+                    <option value="10:00">10:00 am</option>
+                    <option value="11:00">11:00 am</option>
+                    <option value="12:00">12:00 am</option>
+                    <option value="13:00">13:00 pm</option>
+                    <option value="14:00">14:00 pm</option>
+                    <option value="15:00">15:00 pm</option>
+                    <option value="16:00">16:00 pm</option>
+                    <option value="17:00">17:00 pm</option>
+                    <option value="18:00">18:00 pm</option>
+                    <option value="19:00">19:00 pm</option>
+                    <option value="20:00">20:00 pm</option>
+                    <option value="21:00">21:00 pm</option>
+                    <option value="22:00">22:00 pm</option>
+                    <option value="23:00">23:00 pm</option>
                     {/* Agrega más opciones de horario según lo necesites */}
                   </select>
                 </div>
@@ -411,24 +470,29 @@ const EditPage = () => {
             </div>
             <div
               style={{
+                margin: "5px",
                 display: "flex",
                 flexDirection: "column",
-                border: "1px solid black",
+                backgroundColor: "#dddfdb",
+                borderRadius: "10px",
+                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)", // Sombra hacia abajo
               }}
             >
-              <label htmlFor="diasDisponibles" style={{ margin: "0 10px" }}>
+              <label htmlFor="diasDisponibles" style={{ margin: "0 10px", color:"gray" }}>
                 Días disponibles para reservar
               </label>
 
               <div style={{ display: "flex", alignItems: "center" }}>
                 <div style={{ margin: "5px" }}>
-                  <label htmlFor="desde" style={{ margin: "0 5px" }}>
+                  <label htmlFor="desde" style={{ margin: "0 5px", fontSize:"14px" }}>
                     Desde
                   </label>
                   <select
                     id="desde"
+                    value={diaDesde}
                     name="desde"
-                    style={{ marginBottom: "10px", marginLeft: "5px" }}
+                    onChange={(e) => setDiaDesde(e.target.value)} // Actualiza el estado cuando cambia el valor
+                    style={{ marginBottom: "10px", marginLeft: "5px", borderRadius:"5px", border:"none" }}
                   >
                     <option value="lunes">Lunes</option>
                     <option value="martes">Martes</option>
@@ -440,12 +504,14 @@ const EditPage = () => {
                   </select>
                 </div>
 
-                <div style={{ marginLeft: "10px" }}>
+                <div style={{ marginLeft: "5px", fontSize:"14px" }}>
                   <label htmlFor="hasta">Hasta</label>
                   <select
                     id="hasta"
+                    value={diaHasta} // Asocia el valor del estado
                     name="hasta"
-                    style={{ marginBottom: "10px", marginLeft: "5px" }}
+                    onChange={(e) => setDiaHasta(e.target.value)} // Actualiza el estado cuando cambia el valor
+                    style={{ marginBottom: "10px", marginLeft: "5px", borderRadius:"5px", border:"none" }}
                   >
                     <option value="lunes">Lunes</option>
                     <option value="martes">Martes</option>
@@ -460,24 +526,27 @@ const EditPage = () => {
             </div>
             <div
               style={{
+                margin: "5px",
                 display: "flex",
                 flexDirection: "column",
-                border: "1px solid black",
+                backgroundColor: "#dddfdb",
+                borderRadius: "10px",
+                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)", // Sombra hacia abajo
               }}
             >
-              <label htmlFor="diasDisponibles" style={{ margin: "0 10px" }}>
+              <label htmlFor="diasDisponibles" style={{ margin: "0 10px", color:"gray" }}>
                 Días no disponibles para reservar
               </label>
 
               <div style={{ display: "flex", alignItems: "center" }}>
                 <div style={{ margin: "5px" }}>
-                  <label htmlFor="desde" style={{ margin: "0 5px" }}>
+                  <label htmlFor="desde" style={{ margin: "0 5px", fontSize:"14px" }}>
                     Desde
                   </label>
                   <select
                     id="desde"
                     name="desde"
-                    style={{ marginBottom: "10px", marginLeft: "5px" }}
+                    style={{ marginBottom: "10px", marginLeft: "5px", borderRadius:"5px", border:"none" }}
                   >
                     <option value="lunes">Lunes</option>
                     <option value="martes">Martes</option>
@@ -489,12 +558,12 @@ const EditPage = () => {
                   </select>
                 </div>
 
-                <div style={{ marginLeft: "10px" }}>
+                <div style={{ marginLeft: "5px", fontSize:"14px" }}>
                   <label htmlFor="hasta">Hasta</label>
                   <select
                     id="hasta"
                     name="hasta"
-                    style={{ marginBottom: "10px", marginLeft: "5px" }}
+                    style={{ marginBottom: "10px", marginLeft: "5px", borderRadius:"5px", border:"none" }}
                   >
                     <option value="lunes">Lunes</option>
                     <option value="martes">Martes</option>
